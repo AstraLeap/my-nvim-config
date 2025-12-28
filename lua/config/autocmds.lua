30d4b744e455853e11dbb2db79d67e9094d66baa
xwq@pku.edu.cn <karx@PC-k.localdomain>
2025-12-28 17:11:30 +0800

    feat: 配置 OSC 52 剪切板同步

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,18 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+-- 使用 OSC 52 实现跨 SSH 复制
+-- 这样当你按下 y 时，内容会通过终端流传回本地剪切板
+if vim.env.SSH_TTY then
+  vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+    },
+    paste = {
+      ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+    },
+  }
+end
